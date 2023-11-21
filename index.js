9788e231b4fbc33742bb26bfb9bb7a79771bf5be
Sheikh Zubair Ahmed <134803474+zubairahmed23@users.noreply.github.com>
2023-11-21 20:15:24 +0530

    Commit done

--- index.js
+++ index.js
@@ -5,6 +5,38 @@ const PORT = 4000;
 
 app.listen(PORT);
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+var tokens = ["Xej907Nmj8", "WTVx4ZRktk", "jILGrh75Gi", "GaePTJvspg", "FPIP4UwOHP", "OjKVqn7U0H", "0jEeIbKkqa", "Cpfi1zQLCe", "gyD1pgq9pB", "zjwpZZ0Qj8"];
+
+/* SERVER FUNCTIONS {START} */
+
+function randomToken(){
+    let random = Math.floor(Math.random()*tokens.length);
+    let token = tokens[random];
+    return token;
+}
+
+function checkToken(token){
+    for(const t of tokens){
+        if(token == t){
+            return true;
+        }
+    }
+}
+
+/* SERVER FUNCTIONS {END} */
+
 app.get('/', (req, res) => {
-    res.send('Welcome to Aniflics.com');
+    let token = randomToken();
+    res.redirect(`/main/${token}`);
 });
+
+app.get('/main/:token', (req, res) => {
+    if(checkToken(req.params.token)){
+        res.render('main', {token: req.params.token});
+    }else{
+        res.status(498).send(<h1>Invalid Token</h1>);
+    }
+})
